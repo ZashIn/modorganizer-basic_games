59db7ef5ba361b6558b42aa0368ad9027dec45ef
Zash <zash@fantasymail.de>
2021-05-26 16:50:17 +0200

    Add basic_feature module exports:   .IWithModRequirements   .ModRequirement   .ModDataMappingChecker   .ModDataAndRequirementChecker   .ModRequirementMapping

--- basic_features/__init__.py
+++ basic_features/__init__.py
@@ -1,3 +1,7 @@
 # -*- encoding: utf-8 -*-
+# flake8: noqa
 
-from .basic_save_game_info import BasicGameSaveGameInfo  # noqa
+from .basic_save_game_info import BasicGameSaveGameInfo
+from .mod_data_mapping_checker import ModDataMappingChecker
+from .mod_requirement import IWithModRequirements, ModRequirement
+from .mod_requirement_mapping import ModDataAndRequirementChecker, ModRequirementMapping
