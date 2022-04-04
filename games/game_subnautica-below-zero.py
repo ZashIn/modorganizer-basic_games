f2607e44c87d43631573a1c49c67a34e0bbd849e
Zash <zash@fantasymail.de>
2022-04-05 00:18:20 +0200

    Subnautica Below Zero update: based on Subnautica

--- games/game_subnautica-below-zero.py
+++ games/game_subnautica-below-zero.py
@@ -1,19 +1,33 @@
-from ..basic_game import BasicGame
+from __future__ import annotations
+
+import mobase
+
+from . import game_subnautica  # namespace to not load SubnauticaGame here, too!
 
 
-class SubnauticaGame(BasicGame):
+class SubnauticaBelowZeroGame(game_subnautica.SubnauticaGame):
 
     Name = "Subnautica Below Zero Support Plugin"
-    Author = "dekart811"
-    Version = "1.0"
+    Author = "dekart811, Zash"
+    Version = "2.0"
 
     GameName = "Subnautica: Below Zero"
     GameShortName = "subnauticabelowzero"
     GameNexusName = "subnauticabelowzero"
     GameSteamId = 848450
     GameBinary = "SubnauticaZero.exe"
-    GameDataPath = "QMods"
+    GameDataPath = ""
     GameDocumentsDirectory = "%GAME_PATH%"
+    GameSavesDirectory = r"%GAME_PATH%\SNAppData\SavedGames"
 
-    def iniFiles(self):
-        return ["doorstop_config.ini"]
+    _game_extra_save_paths = [
+        r"%USERPROFILE%\Appdata\LocalLow\Unknown Worlds"
+        r"\Subnautica Below Zero\SubnauticaZero\SavedGames"
+    ]
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[
+            mobase.ModDataChecker
+        ] = game_subnautica.SubnauticaModDataChecker(unfold=["BepInExPack_BelowZero"])
+        return True
