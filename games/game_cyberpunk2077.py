b2ef3067979cd9a31047fab23eca5fc62c2c6009
Zash <zash@fantasymail.de>
2022-11-30 18:03:15 +0100

    fix: deploy not recognized on first run + redscript precompile + redmod predeploy + REDprelauncher (buggy)

--- games/game_cyberpunk2077.py
+++ games/game_cyberpunk2077.py
@@ -19,7 +19,12 @@ from ..basic_game import BasicGame
 
 class CyberpunkModDataChecker(BasicModDataChecker):
     default_file_patterns = {
-        "valid": ["archive", "bin", "engine", "r6", "red4ext", "mods"]
+        "valid": ["root", "archive", "engine", "mods", "r6", "red4ext"],
+        "move": {
+            "bin": "root/",
+            # "red4ext": "root/",
+            # "engine": "root/",
+        },
     }
 
 
@@ -69,8 +74,8 @@ class Cyberpunk2077Game(BasicGame):
     GameName = "Cyberpunk 2077"
     GameShortName = "cyberpunk2077"
     GameBinary = "bin/x64/Cyberpunk2077.exe"
-    # GameLauncher = "REDprelauncher.exe"
-    GameDataPath = "%GAME_PATH%"
+    GameLauncher = "REDprelauncher.exe"
+    GameDataPath = "mods"  # "%GAME_PATH%"
     GameDocumentsDirectory = "%USERPROFILE%/AppData/Local/CD Projekt Red/Cyberpunk 2077"
     GameSavesDirectory = "%USERPROFILE%/Saved Games/CD Projekt Red/Cyberpunk 2077"
     GameSaveExtension = "dat"
@@ -90,7 +95,7 @@ class Cyberpunk2077Game(BasicGame):
         "redirect": True,
         "installer": False,
         "exclusions": "archive,*.archive,BonusContent,setup_redlauncher.exe,tools",
-        "linkextensions": "asi,bin,dll,exe,ini,json,kark,lua,redscript",
+        "linkextensions": "asi,bin,dll,exe,ini,json,kark,lua,redscript,reds,toml,ts",
     }
 
     def init(self, organizer: mobase.IOrganizer) -> bool:
@@ -100,7 +105,22 @@ class Cyberpunk2077Game(BasicGame):
         )
         self._featureMap[mobase.ModDataChecker] = CyberpunkModDataChecker()
 
+        self._organizer.onAboutToRun(self._pre_run_callback)
+
+        # self._organizer.onUserInterfaceInitialized()
+        self._set_root_builder_settings()
         return True
+
+    def _set_root_builder_settings(self):
+        # qInfo("RootBuilder on? " + str(self._organizer.isPluginEnabled("RootBuilder")))
+        # TODO: warning about RootBuilder
+        # TODO: set settings only once
+        try:
+            for setting, value in self._root_builder_config.items():
+                self._organizer.setPluginSetting("RootBuilder", setting, value)
+        except RuntimeError:
+            # TODO: show message to install RootBuilder
+            qInfo("ERROR RootBuilder not installed")
 
     def settings(self) -> list[mobase.PluginSetting]:
         return [
@@ -108,42 +128,113 @@ class Cyberpunk2077Game(BasicGame):
                 "skipStartScreen",
                 'Skips the "Breaching..." start screen on game launch.',
                 True,
-            )
+            ),
+            mobase.PluginSetting(
+                "precompile_redscript",
+                "Compiles redscript before game launch (better compatibility).",
+                True,
+            ),
+            mobase.PluginSetting(
+                "predeploy_redmod",
+                "Deploys redmod before game launch (better compatibility with redscript)",
+                True,
+            ),
         ]
 
     def primaryPlugins(self):
         return ["RootBuilder"]
 
-    def executables(self) -> list[mobase.ExecutableInfo]:
-        # TODO: run args = --skip-launcher -skipStartScreen / -modded
-        game_dir = self.gameDirectory()
-        bin_path = QFileInfo(game_dir.absoluteFilePath(self.binaryName()))
-        without_redmod = mobase.ExecutableInfo(self.gameName(), bin_path).withArgument(
-            "--launcher-skip"
+    def _pre_run_callback(self, path: str):
+        if path == self.gameDirectory().absoluteFilePath(self.binaryName()):
+            # TODO: run redmod only if started via "-modded"
+            if self._organizer.pluginSetting(self.name(), "predeploy_redmod"):
+                modded = bool((res := self._redmod_deploy()) and res[0])
+            else:
+                modded = False
+            if self._organizer.pluginSetting(self.name(), "precompile_redscript"):
+                self._redscript_compile(modded)
+        return True
+
+    def _redmod_deploy(self) -> tuple[bool, int] | None:
+        r_path = Path(
+            self.gameDirectory().absoluteFilePath("tools/redmod/bin/redMod.exe")
         )
-        with_redmod = mobase.ExecutableInfo(
-            f"{self.gameName()} + REDmod",
-            bin_path,
-        ).withArgument("-modded")
-        if self._organizer.pluginSetting(self.name(), "skipStartScreen"):
-            without_redmod = without_redmod.withArgument("-skipStartScreen")
-            with_redmod = with_redmod.withArgument("-skipStartScreen")
+        qInfo("REDMOD: " + str(r_path.exists()) + " " + str(r_path))
+        if not r_path.exists():
+            return None
+        return self._organizer.waitForApplication(
+            self._organizer.startApplication("REDmod deploy only")
+        )
 
-        # BUG: redmod + redscript
-        # def run_callback(path: str):
-        #     # TODO: callback with args?
-        #     qInfo(f"run {path} == {bin_path.absoluteFilePath()}")
-        #     if path == bin_path.absoluteFilePath():
-        #         app = self._organizer.startApplication("REDmod deploy only")
-        #         qInfo("run before: " + str(app))
-        #         self._organizer.waitForApplication(app)
-        #     return True
+    def _redscript_compile(self, modded=False) -> tuple[bool, int] | None:
+        """Compiles redscripts (into redmod modded folder with `modded=True`.
+        Returns:
+            if redmod is installed and activated, it returns the run result
+            (see `IOrganizer.waitForApplication`)
+            else None
+        """
+        if not (scc := self._redscript_path()):
+            return None
+        cache_dir = " -customCacheDir r6/cache/modded" if modded else ""
+        return self._organizer.waitForApplication(
+            self._organizer.startApplication(
+                scc,
+                args=f"-compile r6/scripts/{cache_dir}".split(" "),
+                cwd=self.gameDirectory().absolutePath(),
+            )
+        )
 
-        # self._organizer.onAboutToRun(run_callback)
+    def _redscript_path(self, virtual=True) -> str:
+        """Returns empty string if not found."""
+        # TODO: virtual or not? => data path
+        if virtual:
+            tree = self._organizer.virtualFileTree()
+            scc_path = "engine/tools/scc.exe"
+            if tree.exists(scc_path):
+                return self.dataDirectory().absoluteFilePath(scc_path)
+            elif tree.exists(f"root/{scc_path}"):
+                return self.gameDirectory().absoluteFilePath(scc_path)
+            return ""
+        else:
+            scc_paths = self._organizer.findFiles(
+                "engine/tools", lambda f: f == "scc.exe"
+            )
+            if not scc_paths:
+                scc_paths = self._organizer.findFiles(
+                    "root/engine/tools", lambda f: f == "scc.exe"
+                )
+            return scc_paths[0] if scc_paths else ""
 
+    def executables(self) -> list[mobase.ExecutableInfo]:
+        game_name = self.gameName()
+        game_dir = self.gameDirectory()
+        bin_path = game_dir.absoluteFilePath(self.binaryName())
+        skip_start_screen = (
+            " -skipStartScreen"
+            if self._organizer.pluginSetting(self.name(), "skipStartScreen")
+            else ""
+        )
+        execs = []
+        # BUG: launcher does load redscripts
+        if launcher := self.getLauncherName():
+            execs.append(
+                mobase.ExecutableInfo(
+                    Path(launcher).stem,
+                    QFileInfo(game_dir.absoluteFilePath(launcher)),
+                )
+            )
         return [
-            without_redmod,
-            with_redmod,
+            *execs,
+            # Without redmod
+            mobase.ExecutableInfo(game_name, bin_path).withArgument(
+                f"--launcher-skip{skip_start_screen}"
+            ),
+            # With redmod
+            mobase.ExecutableInfo(
+                f"{game_name} + REDmod",
+                bin_path,
+            ).withArgument(f"-modded{skip_start_screen}"),
+            # Redmod deployment
             mobase.ExecutableInfo(
                 "REDmod deploy only",
                 QFileInfo(game_dir.absoluteFilePath("tools/redmod/bin/redMod.exe")),
@@ -152,20 +243,13 @@ class Cyberpunk2077Game(BasicGame):
                 " -rttiSchemaPath="
                 f' "{game_dir.absoluteFilePath("tools/redmod/bin/../metadata.json")}"'
                 " -reportProgress"
-            )
-            # .withArgument("deploy")
-            # .withArgument("-root=")
-            # .withArgument(f'"{game_dir.absolutePath()}"')
-            # .withArgument("-rttiSchemaPath= ")
-            # .withArgument(
-            #     f'"{game_dir.absoluteFilePath("tools/redmod/bin/../metadata.json")}"'
-            # )
-            # .withArgument("-reportProgress")
+            ),
         ]
 
     def executableForcedLoads(self) -> list[mobase.ExecutableForcedLoadSetting]:
         return [
-            mobase.ExecutableForcedLoadSetting(self.binaryName(), lib).withEnabled(True)
+            mobase.ExecutableForcedLoadSetting(exe, lib).withEnabled(True)
+            for exe in [self.binaryName(), self.getLauncherName()]
             for lib in [
                 "bin/x64/version.dll",  # CET
                 "bin/x64/d3d11.dll",  # Red4ext
@@ -180,23 +264,6 @@ class Cyberpunk2077Game(BasicGame):
             BasicGameSaveGame(path.parent)
             for path in Path(folder.absolutePath()).glob(f"**/*.{ext}")
         ]
-
-    def initializeProfile(self, path: QDir, settings: mobase.ProfileSetting):
-        super().initializeProfile(path, settings)
-        qInfo("RootBuilder on? " + str(self._organizer.isPluginEnabled("RootBuilder")))
-
-        # Default settings
-        # qInfo(
-        #     "exclusions: "
-        #     + str(self._organizer.pluginSetting("RootBuilder", "exclusions"))
-        # )
-        try:
-            for setting, value in self._root_builder_config.items():
-                self._organizer.setPluginSetting("RootBuilder", setting, value)
-        except RuntimeError:
-            # TODO: show message to install RootBuilder
-            qInfo("ERROR RootBuilder not installed")
-        # TODO: set settings only once
 
     def iniFiles(self):
         return ["UserSettings.json"]
