4330e3fe2c599f97b9b59aa214a53ee9fc2074e5
Zash <zash@fantasymail.de>
2022-12-05 22:26:04 +0100

    + ModDataChecker

--- games/game_cyberpunk2077.py
+++ games/game_cyberpunk2077.py
@@ -1,10 +1,57 @@
+from pathlib import Path
+
+import mobase
+
+from ..basic_features import BasicModDataChecker
+from ..basic_features.basic_save_game_info import (
+    BasicGameSaveGame,
+    BasicGameSaveGameInfo,
+)
 from ..basic_game import BasicGame
+
+
+class CyberpunkModDataChecker(BasicModDataChecker):
+    default_file_patterns = {
+        "valid": ["root", "archive", "engine", "r6"],
+        "move": {
+            "bin": "root/",
+            "red4ext": "root/",
+            "engine": "root/",
+        },
+    }
+    _extra_root_mapping = {
+        "r6/scripts": "root/r6/",
+        "engine/tools": "root/engine/",
+    }
+
+    def dataLooksValid(
+        self, filetree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        res = super().dataLooksValid(filetree)
+        if res is mobase.ModDataChecker.VALID and any(
+            filetree.exists(path) for path in self._extra_root_mapping
+        ):
+            return mobase.ModDataChecker.FIXABLE
+        return res
+
+    def fix(self, filetree: mobase.IFileTree) -> mobase.IFileTree | None:
+        new_filetree = super().fix(filetree)
+        if new_filetree is None:
+            return None
+        for source, target in self._extra_root_mapping.items():
+            if entry := new_filetree.find(source):
+                parent = entry.parent()
+                new_filetree.move(entry, target)
+                # Remove empty folder
+                if parent is not None and not bool(parent):
+                    parent.detach()
+        return new_filetree
 
 
 class Cyberpunk2077Game(BasicGame):
     Name = "Cyberpunk 2077 Support Plugin"
     Author = "6788, Zash"
-    Version = "1.1.0"
+    Version = "2.0"
 
     GameName = "Cyberpunk 2077"
     GameShortName = "cyberpunk2077"
@@ -20,6 +67,21 @@ class Cyberpunk2077Game(BasicGame):
         r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
         "Game:-Cyberpunk-2077"
     )
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[mobase.SaveGameInfo] = BasicGameSaveGameInfo(
+            lambda p: Path(p or "", "screenshot.png"),
+        )
+        self._featureMap[mobase.ModDataChecker] = CyberpunkModDataChecker()
+        return True
+
+    def listSaves(self, folder) -> list[mobase.ISaveGame]:
+        ext = self._mappings.savegameExtension.get()
+        return [
+            BasicGameSaveGame(path.parent)
+            for path in Path(folder.absolutePath()).glob(f"**/*.{ext}")
+        ]
 
     def iniFiles(self):
         return ["UserSettings.json"]
